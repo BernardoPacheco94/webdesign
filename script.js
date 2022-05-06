cf7a2611920cfc095e541ebad6460748385de296
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-05-06 12:41:50 -0300

    cópia do código base

--- script.js
+++ script.js
@@ -1,47 +1,94 @@
-function startVideoFromCamera() {
-    navigator.mediaDevices.getUserMedia({ video: true}).then(stream => {//(video: true) pode ser separado e passado aqui como parametro, para ser possível alterar as propriedades da exibição do video
-        const videoElement = document.querySelector("#video")
-        videoElement.srcObject = stream
+       let constraintObj = { 
+            audio: false, 
+            video: { 
+                facingMode: "user", 
+                width: { min: 640, ideal: 1280, max: 1920 },
+                height: { min: 480, ideal: 720, max: 1080 } 
+            } 
+        }; 
+        // width: 1280, height: 720  -- preference only
+        // facingMode: {exact: "user"}
+        // facingMode: "environment"
+        
+        //handle older browsers that might implement getUserMedia in some way
+        if (navigator.mediaDevices === undefined) {
+            navigator.mediaDevices = {};
+            navigator.mediaDevices.getUserMedia = function(constraintObj) {
+                let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+                if (!getUserMedia) {
+                    return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+                }
+                return new Promise(function(resolve, reject) {
+                    getUserMedia.call(navigator, constraintObj, resolve, reject);
+                });
+            }
+        }else{
+            navigator.mediaDevices.enumerateDevices()
+            .then(devices => {
+                devices.forEach(device=>{
+                    console.log(device.kind.toUpperCase(), device.label);
+                    //, device.deviceId
+                })
+            })
+            .catch(err=>{
+                console.log(err.name, err.message);
+            })
+        }
 
-    }).catch(error => { console.log(error) })
-}
-
-window.addEventListener("DOMContentLoaded", startVideoFromCamera)
-
-function getConnectedDevices(type, callback) {//verifica quantos dispositivos possíveis estão conectados ao pc
-    navigator.mediaDevices.enumerateDevices()
-        .then(devices => {
-            const filtered = devices.filter(device => device.kind === type);
-            callback(filtered);
+        navigator.mediaDevices.getUserMedia(constraintObj)
+        .then(function(mediaStreamObj) {
+            //connect the media stream to the first video element
+            let video = document.querySelector('video');
+            if ("srcObject" in video) {
+                video.srcObject = mediaStreamObj;
+            } else {
+                //old version
+                video.src = window.URL.createObjectURL(mediaStreamObj);
+            }
+            
+            video.onloadedmetadata = function(ev) {
+                //show in the video element what is being captured by the webcam
+                video.play();
+            };
+            
+            //add listeners for saving video/audio
+            let start = document.getElementById('btnStart');
+            let stop = document.getElementById('btnStop');
+            let vidSave = document.getElementById('vid2');
+            let mediaRecorder = new MediaRecorder(mediaStreamObj);
+            let chunks = [];
+            
+            start.addEventListener('click', (ev)=>{
+                mediaRecorder.start();
+                console.log(mediaRecorder.state);
+            })
+            stop.addEventListener('click', (ev)=>{
+                mediaRecorder.stop();
+                console.log(mediaRecorder.state);
+            });
+            mediaRecorder.ondataavailable = function(ev) {
+                chunks.push(ev.data);
+            }
+            mediaRecorder.onstop = (ev)=>{
+                let blob = new Blob(chunks, { 'type' : 'video/mp4;' });
+                chunks = [];
+                let videoURL = window.URL.createObjectURL(blob);
+                vidSave.src = videoURL;
+            }
+        })
+        .catch(function(err) { 
+            console.log(err.name, err.message);
         });
-}
-
-getConnectedDevices('videoinput', cameras => console.log('Cameras found', cameras));
-
-//Verificando alterações nos dispositivos
-// Updates the select element with the provided set of cameras
-function updateCameraList(cameras) {
-    const listElement = document.querySelector('select#availableCameras');
-    listElement.innerHTML = '';
-    cameras.map(camera => {
-        const cameraOption = document.createElement('option');
-        cameraOption.label = camera.label;
-        cameraOption.value = camera.deviceId;
-    }).forEach(cameraOption => listElement.add(cameraOption));
-}
-
-// Fetch an array of devices of a certain type
-async function getConnectedDevices(type) {
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    return devices.filter(device => device.kind === type)
-}
-
-// Get the initial set of cameras connected
-const videoCameras = getConnectedDevices('videoinput');
-updateCameraList(videoCameras);
-
-// Listen for changes to media devices and update the list accordingly
-navigator.mediaDevices.addEventListener('devicechange', event => {
-    const newCameraList = getConnectedDevices('video');
-    updateCameraList(newCameraList);
-});
+        
+        /*********************************
+        getUserMedia returns a Promise
+        resolve - returns a MediaStream Object
+        reject returns one of the following errors
+        AbortError - generic unknown cause
+        NotAllowedError (SecurityError) - user rejected permissions
+        NotFoundError - missing media track
+        NotReadableError - user permissions given but hardware/OS error
+        OverconstrainedError - constraint video settings preventing
+        TypeError - audio: false, video: false
+        *********************************/
+  
